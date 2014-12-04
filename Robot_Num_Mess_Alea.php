4695a1f53683694709be2091137c70cfd956391a
gtandu <tbrandon91@hotmail.fr>
2014-12-04 22:09:27 +0100

    Fonction du robot qui genere les messages
    Robot complet qui genere un numero et un message et l'insere dans la
    bdd.(Dans la table messageBrute pour être précis)

--- Robot_Num_Mess_Alea.php
+++ Robot_Num_Mess_Alea.php
@@ -21,10 +21,9 @@ function genererNum(){
 		$num='07';
 	}
 	
-	$fin_num = mt_rand(10000000,99999999);
 	// Genere un nombre a 8 chiffre qui correspond a la fin du numero 
-	$num=$num.$fin_num;
-
+	$num=$num.mt_rand(10000000,99999999);
+	
 	return $num;
 }
 
@@ -34,35 +33,57 @@ function genererRep(){
 	$num_QAlea=""; // Numero de la question aleatoire
 	$numRAlea=""; // Numero de la reponse aleatoire generé en fonction de la question
 	
-	$req1=$bd->prepare('SELECT MAX(num_Quest) from question'); // On le selectionne le maximum du nombre de questions
-	$req1->execute();
+	$req=$bd->prepare('SELECT MAX(num_Quest) from question'); // On le selectionne le maximum du nombre de questions
+	$req->execute();
 	
-	$res = $req1->fetch(PDO::FETCH_NUM);
+	$res = $req->fetch(PDO::FETCH_NUM);
 	$numMAX_q = $res[0]; // On recupere le nombre max de question
 	$num_QAlea = mt_rand(1,$numMAX_q); // On genere un nombre entre 1 et le maximum du nombre de questions
 
 	
-	$req2=$bd->prepare('SELECT MAX(num_rep) from reponse where num_Question= :numQ'); // On selectionne le maximum du nombre de reponse en fonction de la question
-	$req2->bindValue(':numQ', $num_QAlea, PDO::PARAM_INT);
-    $req2->execute();
+	$req=$bd->prepare('SELECT MAX(num_rep) from reponse where num_Question= :numQ'); // On selectionne le maximum du nombre de reponse en fonction de la question
+	$req->bindValue(':numQ', $num_QAlea);
+    $req->execute();
 	
-	$res = $req2->fetch(PDO::FETCH_NUM);
+	$res = $req->fetch(PDO::FETCH_NUM);
 	$numMAX_rep = $res[0]; // On recupere le nombre Max de reponse
 	$num_RAlea=mt_rand(1,$numMAX_rep); // On genere un nombre entre 1 et le maximum du nombre de reponse
 	
 	$rep = $num_QAlea.$num_RAlea; // On concatene le Numero de la Question et le numero de la reponse
-
+	
 	return $rep;
 }
 
-// FONCTION POUR AJOUTER LE NUMERO ET LA REPONSE A LA BDD
+function insererMes($num,$mes){
+	global $bd;
+
+	$req=$bd->prepare("SELECT * from MessageBrute where num_recu=:num and corps_mess =:mes"); // On cherche si le numero et le message sont deja dans la table
+	$req->bindValue(':num', $num);
+	$req->bindValue(':mes', $mes);
+	$req->execute();
+	
+	$res = $req->fetch(PDO::FETCH_NUM);
+	
+	if($res[0]==""){ // Si le resultat est vide, le message et le numero ne sont pas dans la table donc on les insert. On ne fait rien sinon.
+		$req=$bd->prepare("INSERT INTO `MessageBrute` (`num_recu`,`corps_mess`) VALUES (:num, :mes)");
+		$req->bindValue(':num', $num);
+		$req->bindValue(':mes', $mes);
+		$req->execute();
+		
+		echo '<br/> Message inserer';
+	}
+	else{
+		echo '<br/> Doublon message';
+	}
+}
 ?>
 
 
 <?php
 // Partie TEST
-	$test=genererNum();
-	echo $test.'<br/>';
-	$test2=genererRep();
-	echo $test2;	
+	$num=genererNum();
+	echo $num.'<br/>';
+	$rep=genererRep();
+	echo $rep;	
+	insererMes($num,$rep);
 ?>
