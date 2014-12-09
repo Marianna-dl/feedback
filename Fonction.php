c604732afcfa9d778bc4be6efd88906688ccc664
Naim-Kissi <naim.kissi@gmail.com>
2014-12-09 21:33:13 +0100

    New Fonction.php

--- Fonction.php
+++ Fonction.php
@@ -22,49 +22,45 @@
 	function seeAllQuestion(){
 		
 
-		try{
+		
 			$db = ConnectionFactory::getFactory()->getConnection();
 			$req=$db->prepare('SELECT * FROM Question');
 
 			$req->execute();
 			$tab=$req->fetchAll(PDO::FETCH_ASSOC); 
-			return (json_encode($tab));
-		}
+			echo json_encode($tab);
 
-		catch(PDOException $e){
-			die('<p> La connexion a échoué </p>');
-
-		}
 
 	}
 
 
 
-	function seeOneQuestion($num){
+	function seeOneQuestion(){
+		
+			$data = json_decode(file_get_contents("php://input"));
+    			$num=$data->id;
 		
 
-		try{
+		
 			$db = ConnectionFactory::getFactory()->getConnection();
 			$req=$db->prepare('SELECT * FROM Question WHERE num_Quest=:num');
 			$req->bindValue(':num',$num); 
 			$req->execute();
 			$tab=$req->fetchAll(PDO::FETCH_ASSOC); 
-			return (json_encode($tab));
-		}
+			echo json_encode($tab);
 
-		catch(PDOException $e){
-			die('<p> La connexion a échoué </p>');
-
-		}
 
 	}
 
 
 
-	function ajouterQuestion($num,$type,$enonce){
-		
+	function ajouterQuestion(){
+		 	$data = json_decode(file_get_contents("php://input"));
+    			$enonce = $data->enonce;
+   			$num=$data->id;
+			$type=$data->type;
 
-		try{
+		
 			$db = ConnectionFactory::getFactory()->getConnection();
 			$req=$db->prepare('INSERT INTO Question (num_Quest, type_Quest, enonce) VALUES (:num,:type,:enonce)');
 			$req->bindValue(':num',$num); 
@@ -72,21 +68,19 @@
 			$req->bindValue(':enonce',$enonce);
 			$req->execute();
 		
-		}
 
-		catch(PDOException $e){
-			die('<p> La connexion a échoué </p>');
-
-		}
 
 	}
 
 
 
-	function modifierQuestion($num,$type,$enonce){
-		
+	function modifierQuestion(){
+		 	$data = json_decode(file_get_contents("php://input"));
+    			$enonce = $data->enonce;
+   			$num=$data->id;
+			$type=$data->type;
 
-		try{
+		
 			$db = ConnectionFactory::getFactory()->getConnection();
 			$req=$db->prepare('UPDATE Question SET type_Quest=:type, enonce=:enonce WHERE num_Quest=:num');
 			$req->bindValue(':num',$num); 
@@ -94,39 +88,34 @@
 			$req->bindValue(':enonce',$enonce);
 			$req->execute();
 			
-		}
 
-		catch(PDOException $e){
-			die('<p> La connexion a échoué </p>');
-
-		}
 
 	}
 
 
-	function supprimerQuestion($num){
+	function supprimerQuestion(){
 		
-
-		try{
+		 	$data = json_decode(file_get_contents("php://input"));
+   			$num=$data->id;
+	
 			$db = ConnectionFactory::getFactory()->getConnection();
 			$req=$db->prepare('DELETE FROM Question WHERE num_Quest=:num');
 			$req->bindValue(':num',$num); 
 			$req->execute();
 			
-		}
 
-		catch(PDOException $e){
-			die('<p> La connexion a échoué </p>');
-
-		}
 
 	}
 
 
-	function ajouterReponse($numQ,$numR,$descrip,$point){
-		
+	function ajouterReponse(){
+		 	$data = json_decode(file_get_contents("php://input"));
+    			$numQ = $data->numQuest;
+   			$numR=$data->numRep;
+			$descrip=$data->description;
+			$point=$data->point;		
 
-		try{
+		
 			$db = ConnectionFactory::getFactory()->getConnection();
 			$req=$db->prepare('INSERT INTO Reponse (num_Question,num_Rep,description,point) VALUES (:numQ,:numR,:descrip,:point)');
 			$req->bindValue(':numQ',$numQ); 
@@ -135,19 +124,18 @@
 			$req->bindValue(':point',$point);
 			$req->execute();
 		
-		}
 
-		catch(PDOException $e){
-			die('<p> La connexion a échoué </p>');
-
-		}
 
 	}
 
-	function modifierReponse($numQ,$numR,$descrip,$point){
-		
+	function modifierReponse(){
+		 	$data = json_decode(file_get_contents("php://input"));
+    			$numQ = $data->numQuest;
+   			$numR=$data->numRep;
+			$descrip=$data->description;
+			$point=$data->point;		
 
-		try{
+		
 			$db = ConnectionFactory::getFactory()->getConnection();
 			$req=$db->prepare('UPDATE Reponse SET description=:descrip, point=:point WHERE num_Question=:numQ AND num_Rep=:numR');
 			$req->bindValue(':numQ',$numQ); 
@@ -156,50 +144,38 @@
 			$req->bindValue(':point',$point);
 			$req->execute();
 			
-		}
 
-		catch(PDOException $e){
-			die('<p> La connexion a échoué </p>');
-
-		}
 
 	}
 
-	function supprimerReponse($numQ,$numR){
+	function supprimerReponse(){
 		
-
-		try{
+		 	$data = json_decode(file_get_contents("php://input"));
+    			$numQ = $data->numQuest;
+   			$numR=$data->numRep;
+		
 			$db = ConnectionFactory::getFactory()->getConnection();
 			$req=$db->prepare('DELETE FROM Reponse WHERE num_Question=:numQ AND num_Rep=:numR');
 			$req->bindValue(':numQ',$numQ); 
 			$req->bindValue(':numR',$numR); 
 			$req->execute();
 			
-		}
 
-		catch(PDOException $e){
-			die('<p> La connexion a échoué </p>');
-
-		}
 
 	}
 
-	function seeReponse($numQ){
+	function seeReponse(){
 		
-
-		try{
+		 	$data = json_decode(file_get_contents("php://input"));
+    			$numQ = $data->numQuest;
+		
 			$db = ConnectionFactory::getFactory()->getConnection();
 			$req=$db->prepare('SELECT * FROM Reponse WHERE num_Question=:numQ');
 			$req->bindValue(':numQ',$numQ); 
 			$req->execute();
 			$tab=$req->fetchAll(PDO::FETCH_ASSOC); 
-			return (json_encode($tab));
-		}
+			echo json_encode($tab);
 
-		catch(PDOException $e){
-			die('<p> La connexion a échoué </p>');
-
-		}
 
 	}
 
