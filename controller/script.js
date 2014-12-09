7cc14be39771146a008456b806fd06370a392019
Marianna-dl <Marianna-dl@users.noreply.github.com>
2014-12-09 22:18:17 +0100

    update formulaire + simplification code

--- controller/script.js
+++ controller/script.js
@@ -61,104 +61,94 @@ var feedbackApp = angular.module('feedbackApp', ['ngRoute','ngAnimate', 'ngTouch
         $scope.title="Paramètres";
 	});
 
-   feedbackApp.controller('questionsController', function($scope, $http) {
-       //on affiche à quelle question on en est (évite les violations d'intégrité bdd)
-        $scope.maxQuestion=parseInt(1);
-        $scope.getMaxQuestion=function(){$http.get("test.php/maxQuest").success(function(data){
-                    if (parseInt(data)>=0)
-                        $scope.maxQuestion=parseInt(data)+parseInt(1);  
-                    else
-                        $scope.maxQuestion=parseInt(1);                   
-                })
-                .error(function() {
-                        console.log('erreur');
-                })
-                                        };
-       $scope.getMaxQuestion();
 
-         $scope.videMessage=function(){
-            $scope.resultMessage = "";
-            $scope.result='';
-       
-       };               
-       
 
-    //On met les questions sur la base de données
-        $scope.valideQuestion=function(){
-                $http.post("test.php/addQuest", {enonce:$scope.questionInput, id:$scope.maxQuestion}).success(function(){
-                    $scope.resultMessage = "La question a été ajoutée !";
-                    $scope.result='alert alert-success';
-                    $scope.getListeQuestions();
-                })
-                .error(function() {
-                    $scope.resultMessage = "Erreur lors de l'ajout de la question";
-                    $scope.result='alert alert-danger';
-                })
-       
-                $scope.questionInput='';
-                $scope.getMaxQuestion(); //Met à jour le nombre de question
-                $scope.getListeQuestions();
-
-    
-       };
-       
-         //permet d'activer les tooltip bootstrap
-         $('[data-toggle="tooltip"]').tooltip();
-	});
-
-   feedbackApp.controller('reponsesController', function($scope, $http) {
-       
-       
-       $scope.listRep=[{reponse:''}];
+ feedbackApp.controller('qcmController', function($scope, $http) {
+     
+        $scope.listRep=[{reponse:''}];
         $scope.listLettre=[{lettre:'A'},{lettre:'B'},{lettre:'C'},{lettre:'D'},{lettre:'E'},{lettre:'F'}];
-
         $scope.nbRepInput = parseInt(1);
         $scope.nbRep = parseInt(1);
-
-        $scope.set = function() {
-            if($scope.nbRep >= $scope.nbRepInput){
-                $scope.listRep.push({reponse:''});
-            }
-            else{
-                $scope.listRep.splice($scope.nbRepInput-1,1);
-            }
-             $scope.nbRepInput = $scope.nbRep;
+     
+  //on affiche à quelle question on en est (évite les violations d'intégrité bdd)
+        $scope.maxQuestion=parseInt(1);
+        $scope.getMaxQuestion=function(){$http.get("test.php/maxQuest")
+            .success(function(data){
+                if (parseInt(data)>=0){
+                    $scope.maxQuestion=parseInt(data)+parseInt(1); 
+                }
+                else{
+                    $scope.maxQuestion=parseInt(1);   
+                }
+            })
+            .error(function() {
+                console.log('erreur');
+            })
         };
-       
-       $scope.valideReponse=function(){
-          /*  console.log($scope.nbRepInput);
-            console.log($scope.numQuestionInput);
-            console.log($scope.listRep[0].reponse);
-            console.log($scope.listRep[1].reponse);*/
-  
-           
-                for(var i=0; i<$scope.nbRepInput;i++){
-                if($scope.listRep[i].reponse!=''){
-                $http.post("test.php/addRep", {description:$scope.listRep[i].reponse, numQuest:$scope.numQuestionInput, numRep:$scope.listLettre[i].lettre}).success(function(data){
-                  $scope.resultMessage = "La réponse a été ajoutée !";
-                  $scope.result='alert alert-success';
-                    $scope.getListeReponses();
-  
-                })
-                .error(function() {
-                    $scope.resultMessage = "Erreur lors de l'ajout de la réponse";
-                    $scope.result='alert alert-danger';
-                })
-            }
-            $scope.listRep[i].reponse="";
-                
-       }
-            $scope.numQuestionInput="";
-            $scope.getListeReponses();
-
-       };
+     
+        $scope.getMaxQuestion();
         $scope.videMessage=function(){
             $scope.resultMessage = "";
             $scope.result='';
        
-       };
+        };         
+     
+     //On met les questions sur la base de données
+        $scope.valideQcm=function(){
+            console.log($scope.maxQuestion);
+            
+            if($scope.questionInput!=''){
+                $http.post("test.php/addQuest", {enonce:$scope.questionInput, id:$scope.maxQuestion})
+                    .success(function(){
+                        for(var i=0; i<$scope.nbRepInput;i++){
+                            if($scope.listRep[i].reponse!=''){
+                                console.log($scope.maxQuestion);
+                                $http.post("test.php/addRep", {description:$scope.listRep[i].reponse, numQuest:$scope.maxQuestion, numRep:$scope.listLettre[i].lettre})
+                                    .success(function(data){
+  
+                                    })
+                                    .error(function() {
+                                        console.log("Erreur lors de l'ajout de la réponse");
+                                    })
+                            }
+                            $scope.listRep[i].reponse="";
+                
+                        }
+                        $scope.resultMessage = "Les questions et les réponses ont été ajoutées !";
+                        $scope.result='alert alert-success';
+                        $scope.getMaxQuestion(); //Met à jour le nombre de question
+                        $scope.getListeQuestions();
+                        $scope.getListeReponses();
+                        $scope.numQuestionInput="";
+                        $scope.questionInput='';
+                    })
+                    .error(function() {
+                        $scope.resultMessage = "Erreur lors de l'ajout des questions/réponses";
+                        $scope.result='alert alert-danger';
+                    })
+            }
 
-	});
+        };    
+     
+        $scope.set = function() {
+            if($scope.nbRep>0){
+                if($scope.nbRep >= $scope.nbRepInput){
+                    $scope.listRep.push({reponse:''});
+                }
+                else{
+                    $scope.listRep.splice($scope.nbRepInput-1,1);
+                }
+                $scope.nbRepInput = $scope.nbRep;
+            }
+            else{
+                $scope.nbRep=parseInt(1);;
+            }
+        };  
+          //permet d'activer les tooltip bootstrap
+         $('[data-toggle="tooltip"]').tooltip();
+ 
+ });
+
 
 
 
