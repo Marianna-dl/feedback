8577975cf26d57dc376e1bdd6102d04f6b03f02d
gtandu <tbrandon91@hotmail.fr>
2014-12-22 18:36:31 +0100

    Update robotGenererCorrige
    Correction de la fonction inserermessage

--- model/RobotGenereDB.php
+++ model/RobotGenereDB.php
@@ -116,30 +116,46 @@ class RobotThread extends Thread {
 	}
 	
  
-	function insererMes($num,$mes){
+	function insererMes(){
 		$bd = ConnectionFactory::getFactory()->getConnection();
 		
+		$num = $this->genererNum(); // Genere un numero
+		$mes = $this->genererRep(); // Genere une réponse
+		
+		$erreur= mt_rand(0,9); 
+		$erreur=((($erreur - 9)*-1)/9)*100; // Calcul un pourcentage d'erreur
+		
 		try{
+			
+			if($erreur <= 15){ // Le pourcentage d'erreur doit etre inferieur ou egale a 15% pour genere un message incorrecte
+				$numAlea= mt_rand(0,30);
+				$chara = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+				$mesAlea = mt_rand(0,25);
+				$mes = $numAlea.substr($chara,$mesAlea,1);
+			}
+				
 			$req=$bd->prepare("SELECT * from messagebrute where num_recu=:num and corps_mess =:mes"); // On cherche si le numero et le message sont deja dans la table
 			$req->bindValue(':num', $num);
 			$req->bindValue(':mes', $mes);
 			$req->execute();
-			
+				
 			$res = $req->fetch(PDO::FETCH_NUM);
-			
+				
 			if($res[0]==""){ // Si le resultat est vide, le message et le numero ne sont pas dans la table donc on les insert. On ne fait rien sinon.
 				$req=$bd->prepare("INSERT INTO `messagebrute` (`num_recu`,`corps_mess`,`date_entree`) VALUES (:num, :mes, NOW())");
 				$req->bindValue(':num', $num);
 				$req->bindValue(':mes', $mes);
 				$req->execute();
-				
+					
+				echo 'Numero : '.$num ;
+				echo '<br/> Reponse envoye : '.$mes ;
 				echo '<br/> Message inserer';
 			}
 			else{
 				echo '<br/> Doublon message';
 			}
 		}
-		
+			
 		catch(PDOException $e){
 			die('<p>Erreur['.$e->getCode().'] : '.$e->getMessage().'</p>');
 		}
