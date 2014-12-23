d357f48359e910348ed91d14d898a1cb71fcee02
tam <ttnguyen183@gmail.com>
2014-12-23 18:50:34 +0100

    update parametre (controller+view), still workin' on angular & js

--- controller/parametreController.js
+++ controller/parametreController.js
@@ -1,10 +1,32 @@
    angular.module('feedbackApp').controller('parametresController', function($scope) {
         $scope.title="Paramètres";
-       $scope.couleur="#cab";
-        $scope.style="color:"+$scope.couleur+";";
-       
-       
-       
-       
+        //Cadre Couleur+Text
+        $scope.couleurCadre="#4DFFE7";
+        $scope.sizeCadre="3em";
+
+        //BG général
+        $scope.couleurBG="#2E89FF";
+
+        //Body BG+Text
+        $scope.couleurBody="#FF983D";
+        $scope.sizeBody="2em";
+
+
+        $scope.styleCadre="color:"+$scope.couleurCadre+";"
+                          +"font-size:"+$scope.sizeCadre+";"
+                          ;
+
+        $scope.styleBG="color:"+$scope.couleurBG+";"
+                          ;
+
+        $scope.styleBody="color:"+$scope.couleurBody+";"
+                          +"font-size:"+$scope.sizeBody+";"
+                          ;
+
 	});
 
+angular
+    .module('app', ['angularFileUpload'])
+    .controller('AppController', function($scope, FileUploader) {
+        $scope.uploader = new FileUploader();
+    });
