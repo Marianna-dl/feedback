1179994ec01d757e7c5410f255a83246e727dc57
tam <ttnguyen183@gmail.com>
2014-12-24 23:46:39 +0100

    upd param

--- controller/parametreController.js
+++ controller/parametreController.js
@@ -1,23 +1,9 @@
    angular.module('feedbackApp').controller('parametresController', function($scope) {
         $scope.title="Paramètres";
-        //Cadre Couleur+Text
-        $scope.couleurCadre="#4DFFE7";
-        $scope.sizeCadre="3em";
-
-        //BG général
-        $scope.couleurBG="#2E89FF";
 
         //Body BG+Text
         $scope.couleurBody="#FF983D";
         $scope.sizeBody="2em";
-
-
-        $scope.styleCadre="color:"+$scope.couleurCadre+";"
-                          +"font-size:"+$scope.sizeCadre+";"
-                          ;
-
-        $scope.styleBG="color:"+$scope.couleurBG+";"
-                          ;
 
         $scope.styleBody="color:"+$scope.couleurBody+";"
                           +"font-size:"+$scope.sizeBody+";"
