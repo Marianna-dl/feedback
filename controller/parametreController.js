79c30fb9b4469e4358eb0917022c67984e11d538
thanhtamnguyen <ttnguyen183@gmail.com>
2015-01-10 19:07:11 +0100

    param

--- controller/parametreController.js
+++ controller/parametreController.js
@@ -1,4 +1,4 @@
-   angular.module('feedbackApp').controller('parametresController', function($scope) {
+angular.module('feedbackApp').controller('parametresController', function($scope) {
         $scope.title="Paramètres";
 	   
         //les variables
@@ -11,30 +11,14 @@
 	    $scope.colorBadAnswer="";
 	    $scope.colorFirst="";
 	    $scope.colorSecond="";
-
-        $scope.attribut = ['font-family', 'font-size', 'color'];
-      	$scope.valueQuestion = [$scope.fontQuestion, $scope.sizeQuestion, $scope.colorQuestion];
-	    $scope.valueGoodAnswer = [$scope.fontAnswer, $scope.sizeAnswer, $scope.colorGoodAnswer];
-	    $scope.valueBadAnswer = [$scope.fontAnswer, $scope.sizeAnswer, $scope.colorBadAnswer];
-	   
-	   	$scope.styleQuestion = function(attribut,valueQuestion) {
-          return { attribut: valueQuestion };
-      	}
-		$scope.styleGoodAnswer = function(attribut,valueGoodAnswer) {
-          return { attribut: valueGoodAnswer };
-      	}
-		$scope.styleBadAnswer = function(attribut,valueBadAnswer) {
-          return { attribut: valueBadAnswer };
-      	}
-	   
-	   $scope.styleTest = function() {
-          return { 	"font-family": $scope.fontQuestion
-				 	//"font-size": $scope.sizeQuestion"px";
-				  	//"color": $scope.colorQuestion
-				 };
-      }
 	});
 
-    angular.module('feedbackApp').controller('imageController', function($scope, FileUploader) {
+angular.module('feedbackApp').controller('imageController', function($scope, FileUploader) {
         $scope.uploader = new FileUploader();
     });
+
+angular.module('feedbackApp').controller('paramRenduController', function($scope,$http) {
+
+	   /* $scope.colorFirst="";
+	    $scope.colorSecond="";*/
+});
--- model/Parametres.php
+++ model/Parametres.php
@@ -1,28 +1,40 @@
 <?php
-
 class Parametres{
-
-	public function setQuestion($attribut,$value){
-		$_SESSION['question'][$attribut]=value;
+	public function __construct()
+	{	
+		$param=[$paramQuest, $paramRep];
+		$paramQuest=['font-family' => '', 'font-size' => '', 'color' => ''];
+		$paramRep=['font-family' => '', 'font-size' => '', 'color1' => '', 'color2' => ''];
 	}
-
-	public function setGoodAnswer($attribut,$value){
-                $_SESSION['goodAnswer'][$attribut]=value;
-        }
-
-	public function getParametresQuestion($attribut){
-		return $_SESSION['question'][$attribut];
+	
+	public function setParamQuestion($v1,$v2,$v3){
+		$param[$paramQuest]['font-family']=$v1;
+		$param[$paramQuest]['font-size']=$v2;
+		$param[$paramQuest]['color']=$v3;		
 	}
-
-	public function getParametresGoodAnswer($attribut){
-                return $_SESSION['goodAnswer'][$attribut];
-        }
-
-	public function getParametresWrongAnswer{
-                return $_SESSION['wrongAnswer'][$attribut];
-        }
-
+	
+	public function setParamReponse($v1,$v2,$v3,$v4){
+		$param[$paramRep]['font-family']=$v1;
+		$param[$paramRep]['font-size']=$v2;
+		$param[$paramRep]['color1']=$v3;		
+		$param[$paramRep]['color2']=$v4;		
+	}
+	
+	public function getParamQuestion($attribut){
+		//test		
+		$param[$paramQuest]['font-family']='Times';
+		$param[$paramQuest]['font-size']=44;
+		$param[$paramQuest]['color']='blue';
+		return $param[$paramQuest];
+	}
+	
+	public function getParamReponse($attribut){
+		//test
+		$param[$paramRep]['font-family']='Raleway';
+		$param[$paramRep]['font-size']=22;
+		$param[$paramRep]['color1']='green';		
+		$param[$paramRep]['color2']='red';
+        return $param[$paramRep];
+    }
 }
-
-
 ?>
